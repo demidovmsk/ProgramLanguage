5fec43a69de28e93bfb219d90359421e4e137967
Dima <demidov_msk@mail.ru>
2022-12-23 19:47:48 +0300

    finish

---
+++ lesson2/lesson2ex2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
+
+Random random = new Random();
+int number = random.Next(100,1000);
+
+Console.WriteLine(number);
+
+int firstDigit = number / 100;
+int LastDigit = number % 10;
+int newDitgit = firstDigit * 10 + LastDigit;
+
+Console.WriteLine(newDitgit);
---
+++ lesson2/lesson2ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая будет принимать на вход два числа и выводить, является ли первое число кратным второму.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления. 34, 5 -> не кратно, остаток 4 16, 4 ->кратно
+
+Random random = new Random();
+
+int randomNumber1 = random.Next(10, 100);
+int randomNumber2 = random.Next(1, 10);
+
+Console.WriteLine(randomNumber1);
+Console.WriteLine(randomNumber2);
+
+if ((randomNumber1 % randomNumber2) != 0)
+{
+    Console.WriteLine(randomNumber1 % randomNumber2);
+}
+else
+{
+    Console.WriteLine("кратно");
+}
